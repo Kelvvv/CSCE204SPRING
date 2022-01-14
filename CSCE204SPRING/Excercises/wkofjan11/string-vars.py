0005577eb609d02d3b53ba9bcb96f4aa74893673
Kelvvv <70305854+Kelvvv@users.noreply.github.com>
2022-01-13 21:19:52 -0500

    Add files via upload

---
+++ CSCE204SPRING/Excercises/wkofjan11/string-vars.py
@@ -0,0 +1,13 @@
+#Author Kelvin Keller 
+
+firstname = "Kelvin"
+lastname = "Keller"
+
+print("Welcome " + firstname + " " + lastname + "!")
+
+print(f"Welcome {firstname} {lastname}!")
+
+print("I love chees\nBut I dont like milk.")
+
+print(f"""I love cheese 
+But I dont't like milk""")
