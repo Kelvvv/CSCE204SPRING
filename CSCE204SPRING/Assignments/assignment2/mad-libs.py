0616fa13632922e3cbbd96b987f01a047aa514da
Kelvvv <70305854+Kelvvv@users.noreply.github.com>
2022-01-15 23:52:32 -0500

    Add files via upload

---
+++ CSCE204SPRING/Assignments/assignment2/mad-libs.py
@@ -0,0 +1,17 @@
+#Author Kelvin Keller
+firstnum = input ("Enter a number: ")
+secondnum = input ("Enter a number: ")
+animal = input ("Enter an animal: ")
+liquid = input ("Enter a liquid: ")
+thirdnum = input ("Enter a number:")
+firstadj = input ("Enter an adjective: ")
+sweetfood = input ("Enter a sweet food:: ")
+fourthnum = input ("Enter a number: ")
+secondadj = input ("Enter an adjective: ")
+
+print (f"""First, preheat the oven to {firstnum} degrees. Then beat {secondnum} {animal} egg(s) with a cup of <liquid>.
+Next add {thirdnum} teaspoon(s) of baking soda and mix with eggs and {liquid} until it reaches a {firstadj} texture.
+Don't forget the {sweetfood}! Pour into a 9 by 9 pan and bake for {fourthnum} minute(s) or until the cake is {secondadj}.First, preheat the oven to {firstnum} degrees. Then beat {secondnum} {animal} egg(s) with a cup of {liquid}.
+Next add {thirdnum} teaspoon(s) of baking soda and mix with eggs and {liquid} until it reaches a {firstadj} texture.
+Don't forget the {sweetfood}! Pour into a 9 by 9 pan and bake for {fourthnum} minute(s) or until the cake is {secondadj}.
+""")
