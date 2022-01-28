6460fb24a52d69d23941613b95edd37c051676ce
Kelvvv <70305854+Kelvvv@users.noreply.github.com>
2022-01-28 14:06:58 -0500

    Add files via upload

---
+++ CSCE204SPRING/Excercises/wkofjan24/dating.py
@@ -0,0 +1,4 @@
+status = input("Enter relationship status: ").lower().strip()
+
+if (status != "married"):
+    print("You can come!")
--- CSCE204SPRING/Excercises/wkofjan24/speed.py
+++ CSCE204SPRING/Excercises/wkofjan24/speed.py
@@ -2,11 +2,21 @@
 speed = float(input("Enter Speed: "))
 ticketPrice = 0
 
-if speed >= 90:
-    print ("Time for a ticket")
+if speed >= 100:
+    print ("Time to stay overnight")
+    ticketPrice = 20 * (speed - 70)
+    print("You are charged ${ticketPrice}")
+elif speed >= 90:
+    print("Time for a ticket")
+    ticketPrice = 10 * (speed - 70)
+    print("You are charged ${ticketPrice}")
 elif speed >= 80:
     print("Warning!")
 elif speed > 70:
-    print("Slow down")
-elif speed > 60:
-    print 
+    print("Slow down!")
+elif speed > 70:
+    print("slow down")
+elif speed > 70:
+    print ("slow down")
+else:
+    print("Too slow time for a ticket")
