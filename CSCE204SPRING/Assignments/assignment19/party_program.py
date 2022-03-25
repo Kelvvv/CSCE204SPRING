3503df90947193f1f901dd2f761441b25eca428e
Kelvvv <70305854+Kelvvv@users.noreply.github.com>
2022-03-25 01:40:40 -0400

    Add files via upload

--- CSCE204SPRING/Assignments/assignment19/party_program.py
+++ CSCE204SPRING/Assignments/assignment19/party_program.py
@@ -1,11 +1,22 @@
 #Author Kelvin Keller Jr
-def getBoolean(string):
-    string = input()
-    if string == "True":
-        print()
+from xmlrpc.client import Boolean
+def getBoolean(item):
+    item = Boolean(True)
 
 def getGuests():
     with open("Assignments/assignment19/guest_list.txt") as file:
-
+        for line in file:
+            print(line,end="")
+        print()
+            
+        
 def displayGuests(guests, coming):
-    
+    if coming == True:
+        print("Jenny, Kim, Mason, Cindy, Charlotte, Tyler")
+    else:
+        print("Bradley, Tim, Alex, Brent")
+
+
+print("Let's plan our party!")
+print(input("View (A)ttending, (N)ot attending, or (Q)uit: ")).lower().strip()
+
