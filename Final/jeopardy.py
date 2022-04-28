fb21f6f2363d6d3893cbcd9867c9f9f198948188
Kelvvv <70305854+Kelvvv@users.noreply.github.com>
2022-04-28 00:19:24 -0400

    Add files via upload

--- Final/jeopardy.py
+++ Final/jeopardy.py
@@ -1,13 +1,39 @@
 #Author Kelvin Keller
+from question import Question
+import turtle
+turtle.setup(1000,1000)
+pen = turtle.Turtle()
+pen.speed(0)
+pen.pensize(2)
+pen.hideturtle()
+turtle.bgcolor("white")
+style = ("Courier", 10, "bold")
+
 #Will read in the file questions.txt and return a list of question objects.  A sample qustions.txt is attached.  I'd prefer if you made your own to make it fun :)
-def get_Questions():
-    textfile = "Final/questions.txt"
+def getQuestions():
+    questiion = []
+    textfile = "Final/questions.txt" #get it to return a list 
     with open(textfile) as file:
         for line in textfile:
-            #get it to return a list 
-
+            data = line.split(" : ")
+            thequestion = data[0].strip()
+            theanswer  = data[1].strip()
+            userscurrentguess = data[2].strip()
+            wongame = data[3].strip()
+            questiion.append(Question(thequestion, theanswer,userscurrentguess,wongame))
+    return questiion
 def display_Questions(question):
-    #displays the question
+    turtle.write(f"{question}") #displays the question
 
 def display_Answer(question):
-    #Will display what the user has currently guessed, this will initially be all underscores
+    turtle.write(question)    #Will display what the user has currently guessed, this will initially be all underscores
+
+
+#gameboard
+turtle.write(getQuestions, font = style)
+turtle.onscreenclick("")
+
+
+interface = int(turtle.textinput("Jeopardy", "Enter Letter"))
+
+turtle.done()
